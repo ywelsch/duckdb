1cf961681d1eb0ab51e7749c39308e1e7ad30091
Yannick Welsch <yannick@welsch.lu>
2026-03-04 22:54:23 +0100

    Fix BWC condition for 0.9.2 views

--- src/catalog/catalog_entry/view_catalog_entry.cpp
+++ src/catalog/catalog_entry/view_catalog_entry.cpp
@@ -17,15 +17,11 @@ namespace duckdb {
 void ViewCatalogEntry::Initialize(CreateViewInfo &info) {
 	query = std::move(info.query);
 	this->aliases = info.aliases;
-	if (!info.types.empty() && !info.names.empty()) {
+	if (!info.types.empty()) {
 		bind_state = ViewBindState::BOUND;
 		view_columns = make_shared_ptr<ViewColumnInfo>();
 		view_columns->types = info.types;
 		view_columns->names = info.names;
-		if (info.types.size() != info.names.size()) {
-			throw InternalException("Error creating view %s - view types / names size mismatch (%d types, %d names)",
-			                        name, info.types.size(), info.names.size());
-		}
 	}
 	this->temporary = info.temporary;
 	this->sql = info.sql;
